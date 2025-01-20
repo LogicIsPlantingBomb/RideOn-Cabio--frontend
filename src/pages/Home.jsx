1432387eb403f177a9a4b0abe43d211e88a9b98e
LogicIsPlantingBomb <logicunit64@gmail.com>
2025-01-21 01:15:15 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react"
+import logo from "./images/RideOnCabio-removebg-preview.png"
 const Home = ()=>{
 	return(
 		<div>
-			<h1>This is Home</h1>
+			<div className="h-screen p-5 w-full bg-red-400 flex justify-between flex-col w-full" >
+				<img className="w-16" src={logo} alt="not loaded" />
+				<div className="bg-white">
+					<h2>Get Started with RideOn Cabio</h2>
+					<button>Continue</button>
+				</div>
+			</div>
 		</div>
 	)
 }
