4728138730b300dfca61a5e64f5ae81d125301e6
LogicIsPlantingBomb <logicunit64@gmail.com>
2025-01-23 01:45:03 +0530

    UserLoginAndHoe C

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,32 @@
-import React from "react"
-import logo from "./images/RideOnCabio-removebg-preview.png"
-const Home = ()=>{
-	return(
-		<div>
-			<div className="h-screen p-5 w-full bg-red-400 flex justify-between flex-col w-full" >
-				<img className="w-16" src={logo} alt="not loaded" />
-				<div className="bg-white">
-					<h2>Get Started with RideOn Cabio</h2>
-					<button>Continue</button>
-				</div>
-			</div>
-		</div>
-	)
-}
+import React from "react";
+import logo from "./images/RideOnCabio-removebg-preview.png";
+import trafficLight from "./images/trafficlight.png";
+import {Link} from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div>
+      <div
+        className="h-screen w-full flex justify-between flex-col"
+        style={{
+          backgroundImage: `url(${trafficLight})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+	  backgroundPosition:"center"
+        }}
+      >
+        <img className="w-32 ml-6" src={logo} alt="not loaded" />
+        <div className="bg-white py-5 px-5">
+          <h2 className="text-2xl font-bold">Get Started with RideOn Cabio</h2>
+          <Link to="/users-login" className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-2">
+            Continue
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
+
--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,8 +1,35 @@
 import React from "react"
+import logo from "./images/RideOnCabio-removebg-preview.png"
+import {Link} from "react-router-dom"
 const UserLogin = ()=>{
 	return (
-		<div>
-			<h1>This is User Login</h1>
+		<div className="p-7 h-screen flex flex-col justify-between">
+			<div>
+			<img className="w-32 mb-10" src={logo} alt="notloaded" />
+			<form>
+				<h3 className="text-xl mb-2" >What's your email</h3>
+				<input className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+					required 
+					type="email" 
+					placeholder="email@example.com"
+				/>
+				
+				<h3 className="text-xl mb-2" >Enter the Password</h3>
+				<input className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" 
+					required 
+					type="password"
+					placeholder="password" 
+				/>
+				
+				<button className="bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base">Login</button>
+				
+			</form>
+			<p className="text-center">New here?</p><Link className="text-blue-600" to="/users-register">Create new Account</Link>
+			</div>
+			<div>
+				<Link className="bg-lime-600 flex items-center justify-center text-white font-semibold mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" to="/captain-register">Sign in as Captain</Link>
+			</div>
+
 		</div>
 	)
 }
