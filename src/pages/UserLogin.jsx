001daed89cd94ea3a3f3b91ac90bee1aac3b6bcd
LogicIsPlantingBomb <logicunit64@gmail.com>
2025-01-25 12:48:10 +0530

    UserLoginDone

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,14 +1,30 @@
-import React from "react"
+import {React,useState} from "react"
 import logo from "./images/RideOnCabio-removebg-preview.png"
 import {Link} from "react-router-dom"
 const UserLogin = ()=>{
+	const [email,setEmail] = useState('');
+	const [password,setPassword] = useState('');
+	const submitHandler = (e)=>{
+		e.preventDefault();
+		console.log(email,password);
+		setEmail("");
+		setPassword("");
+	}
+	const [userData,setUserData] = useState({});
+
 	return (
 		<div className="p-7 h-screen flex flex-col justify-between">
 			<div>
 			<img className="w-32 mb-10" src={logo} alt="notloaded" />
-			<form>
+			<form onSubmit={(e)=>{
+				submitHandler(e);
+			}}>
 				<h3 className="text-xl mb-2" >What's your email</h3>
 				<input className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+					value={email}
+					onChange={(e)=>{
+						setEmail(e.target.value);
+					}}
 					required 
 					type="email" 
 					placeholder="email@example.com"
@@ -16,6 +32,10 @@ const UserLogin = ()=>{
 				
 				<h3 className="text-xl mb-2" >Enter the Password</h3>
 				<input className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base" 
+					value={password}
+					onChange={(e)=>{
+						setPassword(e.target.value);
+					}}
 					required 
 					type="password"
 					placeholder="password" 
